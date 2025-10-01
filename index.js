34d52da3da95a2deb969f596e9ebda9d8dd387a9
Juan Carlos Pevé <juancp156@gmail.com>
2025-10-01 20:18:51 -0300

    se agrego funciones importadas

--- index.js
+++ index.js
@@ -1,60 +1,8 @@
+import { agregarProducto, eliminarProducto, obtenerProductos } from "./fakestoreApi.js";
+
 // Constantes
 const args = process.argv.slice(2);
 const [method, path, ...data] = args;
-
-// URL de la API FakeStore
-const API_URL = "https://fakestoreapi.com/products";
-
-// ----------------------------------------------------------------------------
-// Funciones para cada tipo de petición
-// ----------------------------------------------------------------------------
-
-// Función para obtener uno o más productos
-async function obtenerProductos(id = "") {
-  try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "GET",
-    });
-    const products = await response.json();
-    console.log(products);
-  } catch (error) {
-    console.error("Error al obtener los productos:", error);
-  }
-}
-
-// Función para crear un producto nuevo
-async function agregarProducto(title, price, category) {
-  try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        price,
-        category,
-      }),
-    });
-    const result = await response.json();
-    console.log("Producto creado con éxito:", result);
-  } catch (error) {
-    console.error("Error al crear el producto:", error);
-  }
-}
-
-// Función para eliminar un producto
-async function eliminarProducto(id) {
-  try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "DELETE",
-    });
-    const result = await response.json();
-    console.log("Producto eliminado con éxito:", result);
-  } catch (error) {
-    console.error("Error al eliminar el producto:", error);
-  }
-}
 
 // ----------------------------------------------------------------------------
 // Lógica para ejecutar la función correcta según los argumentos
@@ -62,13 +10,13 @@ async function eliminarProducto(id) {
 const run = () => {
   if (method === "GET" && path === "products") {
     obtenerProductos();
-  } else if (method === "GET" && path.startsWith("products/")) {
+  } else if (method === "GET" && path.includes("products/")) {
     const id = path.split("/")[1];
     obtenerProductos(id);
   } else if (method === "POST" && path === "products") {
     const [title, price, category] = data;
     agregarProducto(title, Number(price), category);
-  } else if (method === "DELETE" && path.startsWith("products/")) {
+  } else if (method === "DELETE" && path.includes("products/")) {
     const id = path.split("/")[1];
     eliminarProducto(id);
   } else {
